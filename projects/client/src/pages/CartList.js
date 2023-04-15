540797a7772e4ab56bb8d7fa7927e42a9fcc8ce1
robi221 <robismicel@gmail.com>
2023-04-15 18:56:36 +0700

    OG-19

--- projects/client/src/pages/CartList.js
+++ projects/client/src/pages/CartList.js
@@ -95,13 +95,6 @@ const CartList = () => {
                       <span className="font-bold text-sm">
                         {val.product_name}
                       </span>
-                      {/* <span className="text-red-500 text-xs">Apple</span> */}
-                      {/* <a
-                        href="#"
-                        className="font-semibold hover:text-red-500 text-gray-500 text-xs"
-                      >
-                        Remove
-                      </a> */}
                       <a
                         href="#"
                         className="font-semibold hover:text-red-500 text-gray-500 text-xs"
@@ -136,28 +129,6 @@ const CartList = () => {
                       +
                     </button>
                   </div>
-
-                  {/* <div className="flex justify-center w-1/5">
-                    <svg
-                      className="fill-current text-gray-600 w-3"
-                      viewBox="0 0 448 512"
-                    >
-                      <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                    </svg>
-
-                    <input
-                      className="mx-2 border text-center w-8"
-                      type="text"
-                      value={val.qty}
-                    />
-
-                    <svg
-                      className="fill-current text-gray-600 w-3"
-                      viewBox="0 0 448 512"
-                    >
-                      <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                    </svg>
-                  </div> */}
                   <span className="text-center w-1/5 font-semibold text-sm">
                     Rp. {val.price}
                   </span>
@@ -192,14 +163,14 @@ const CartList = () => {
               </span>
               <span className="font-semibold text-sm"> Rp. {grandtotal}</span>
             </div>
-            {/* <div>
+            <div>
               <label className="font-medium inline-block mb-3 text-sm uppercase">
                 Shipping
               </label>
               <select className="block p-2 text-gray-600 w-full text-sm">
                 <option>Standard shipping - $10.00</option>
               </select>
-            </div> */}
+            </div>
             <div className="py-10">
               <label
                 for="promo"
